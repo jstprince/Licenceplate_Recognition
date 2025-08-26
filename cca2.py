8d9ac1ba331becd1c67402f134bf17ffb68850d0
Langston Pearson <langstonpearson22@gmail.com>
2025-08-26 13:47:10 -0400

    commit

--- cca2.py
+++ cca2.py
@@ -13,3 +13,22 @@ plate_objects_cordinates = []
 plate_like_objects = []
 fig, (ax1) = plt.subplots(1)
 ax1.imshow(localization.binary_car_image, cmap='gray')
+
+#regionprops creates a list of properties of all the labled regions 
+
+for region in regionprops(label_image):
+    if region.area < 50:
+        continue
+    minr, minc, maxr, maxc = region.bbox
+    region_height = maxr - minr
+    region_width = maxc - minc
+
+    if region_height >= min_height and region_height <= max_height and region_width >= min_width and region_width <= max_width and region_width > region_height:
+        plate_like_objects.append(localization.binary_car_image[minr:maxr,minc:maxc])
+        
+        plate_objects_cordinates.append((minr,minc,maxr,maxc))
+
+        rectBorder = patches.Rectangle((minc, min), maxc - minc, maxr - minr, edgecolor = 'red', linewidth = 2, fill = False)
+        ax1.add_patch(rectBorder)
+
+plt.show()
